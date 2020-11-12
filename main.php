f92100e75b09dd8147d623e5c687e24925aa68fa
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-11-12 20:53:53 +0200

    Start integrating

---
+++ main.php
@@ -0,0 +1,8 @@
+<?php
+
+use HackAssembler\Assembler;
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+
+$assembler = new Assembler();
+$assembler->handle();
