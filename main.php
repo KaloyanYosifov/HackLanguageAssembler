c780d8365bb2c2adde03d7438c9541bb0d84fc65
Kaloyan Yosifov <kaloqn665@gmail.com>
2020-11-14 22:46:55 +0200

    Support command line form

--- main.php
+++ main.php
@@ -2,7 +2,18 @@
 
 use HackAssembler\Assembler;
 
+if (php_sapi_name() !== PHP_SAPI) {
+    echo 'Please run this in the command line!';
+    exit(1);
+}
+
+
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
+if ($argc !== 2) {
+    echo 'Please specify the file we should translate!';
+    exit(1);
+}
+
 $assembler = new Assembler();
-$assembler->handle(__DIR__ . '/tests/add.asm');
+$assembler->handle($argv[1]);
